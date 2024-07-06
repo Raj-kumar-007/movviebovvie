4ca892394568b19ae4bb282532239f2d2e06416d
Sonali <59190002+SonaliDandoriya@users.noreply.github.com>
2024-07-06 16:11:30 +0530

    Removing commented code

--- src/router/actions/loginAction.ts
+++ src/router/actions/loginAction.ts
@@ -2,12 +2,6 @@ import { redirect } from "react-router-dom";
 import { parseFormData } from "../../utils/parseFormData";
 
 export async function loginAction({ request }) {
-  // const contentType = request.headers.get("content-type");
-  // const isJsonRequest = contentType === "application/json";
-  // const data = isJsonRequest
-  //   ? await request.json()
-  //   : parseFormData(await request.formData());
-
   const data = parseFormData(await request.formData());
 
   const { method } = request;
